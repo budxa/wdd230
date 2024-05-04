c9605e196a948d24b924da036c52d37a2160c508
budxa <jeraldmudzingwa@gmail.com>
2024-05-04 11:39:03 +0200

    home-page-2.1

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -7,3 +7,11 @@ let currentYear = `${year}`;
 document.querySelector("#year").textContent = currentYear;
 
 document.getElementById("currentdate").textContent = document.lastModified;
+
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+hamButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+    hamButton.classList.toggle("open");
+})
