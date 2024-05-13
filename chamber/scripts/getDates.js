82ceef67c92b422ebf4968deb1e4e26b55db3176
budxa <jeraldmudzingwa@gmail.com>
2024-05-13 17:12:37 +0200

    discovery -page-1.1

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -17,3 +17,23 @@ hamButton.addEventListener('click', () => {
 	navbar.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+
+
+let lastVisit = localStorage.getItem("last-visit");
+const lastVisitElement = document.querySelector('#lastVisith');
+
+if (lastVisitElement != null) {
+    if (lastVisit) {
+        let currentDate = new Date();
+        let previousDate = new Date(lastVisit);
+        let timeDiffernce = currentDate.getTime() - previousDate.getTime();
+
+        let differnceInDays = Math.floor(timeDiffernce / (1000 * 60 * 60 * 24));
+
+        lastVisitElement.innerHTML = `You visited this webpage ${differnceInDays} day(s) ago!`;
+	} else {
+		lastVisitElement.innerHTML = `Welcome! Let us know if you have any questions.`;
+	}
+
+    localStorage.setItem("last-visit", new Date());
+};
